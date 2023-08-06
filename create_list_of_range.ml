2a3fb15f1547c8a20470d3fa928d7509992fbd40
Ethan Zhang <ezsland05@gmail.com>
2023-08-05 22:42:06 -0700

    fixed range

--- create_list_of_range.ml
+++ create_list_of_range.ml
@@ -1,6 +1,11 @@
+(* If first argument is greater than second, produce a list in decreasing order. *)
+
 let rec range a b =
-    if a = b then [b]
-    else a :: range (a + 1) b
+    let rec aux a b =
+        if a = b then [b]
+        else a :: range (a + 1) b
+    in
+    if a > b then aux b a else aux a b
 
 (*
 # range 4 9;;
